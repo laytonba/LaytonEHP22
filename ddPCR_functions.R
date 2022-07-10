fcb2f0420921e990c6ce92c74117541de1e61035
laytonba <106122319+laytonba@users.noreply.github.com>
2022-07-10 15:06:38 -0700

    Add files via upload

--- ddPCR_functions.R
+++ ddPCR_functions.R
@@ -1,17 +1,38 @@
-# custom functions for use in covid ddPCR data analysis
-# author: B. Layton unless otherwise noted
+# custom functions for use in ddPCR data analysis
+# B. Layton
 
 
 
 # a handy function for pretty figures and legends
-unlog = function(x) {round(10^x, digits = 0)}
+unlog <- function(x) {round(10^x, digits = 0)}
 
 # function for calculating standard error
-se = function(x) {sd(x, na.rm = T)/sqrt(length(x))}
+se <- function(x) sd(x, na.rm = T)/sqrt(length(x))
+
+
 
 #### Custom aggregation functions ####
+# function to count positives in QC samples
+blankMaxFun <- function (x){ # allows for corrections for re-runs when calling results for blank samples
+  goodPlate <- max(x$Plate) # assumes most recent plate is one to use
+  blankMax <- max(x$Positives[which(x$Plate == goodPlate)], na.rm = T)
+  return(blankMax)
+}
+
+# numeric aggregation function to feed into dcast (needed bc mean will be skewed low if any rep is 0):
+# this is no longer needed for most data bc of 1/2 LOD assignment, but comes in handy for maps and OVDL data
+partialMean <- function(x) {
+  if (sum(x, na.rm = T) == 0) {
+    return(0) # if all are 0, return 0
+  } else if (any(x == 0, na.rm = TRUE)) {
+    x <- x[which(x != 0)]
+    return(mean(x, na.rm = T)) # if some are 0, remove 0's and return the mean
+  } else
+    return(mean(x, na.rm = T)) # if no 0's, return the mean
+}
+
 # function for aggregating qualitative results - for use with dcast on QualCall variable only:
-partialQualAggr = function(x) { 
+partialQualAggr <- function(x) { 
   if (all(is.na(x))) {
     return("NA") # if all are NAs - meaning all reps failed QC due to low droplets or all targets 0 - return NA
   } else if (all(str_detect(x, "Negative"), na.rm = TRUE)) {
@@ -24,7 +45,7 @@ partialQualAggr = function(x) {
 }
 
 # function for aggregating QCreason - for use with dcast on QCreason variable only:
-aggrQCreason = function(x) { 
+aggrQCreason <- function(x) { 
   x <- str_replace_na(as.character(x))
   if (all(str_detect(x, "NA"))) {
     return("NA") # if all reps are NA, return NA
@@ -35,7 +56,7 @@ aggrQCreason = function(x) {
 }
 
 # function for pulling out the lower 95% confidence interval from t.test 
-lowerConfInf = function(x) {
+lowerConfInf <- function(x) {
   if (all(is.na(x))) return(NA) # needed to avoid breaking the t.test function
   else {
     nx <- length(x)
@@ -54,7 +75,7 @@ lowerConfInf = function(x) {
 }
 
 # function for pulling out the upper 95% confidence interval from t.test 
-upperConfInf = function(x) {
+upperConfInf <- function(x) {
   if (all(is.na(x))) return(NA) # needed to avoid breaking the t.test function
   else {
     nx <- length(x)
@@ -71,3 +92,4 @@ upperConfInf = function(x) {
     }
   }
 }
+
